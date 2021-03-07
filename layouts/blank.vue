ff9721f72b2fe89562c394760ce32e05ec671c50
jprgr <jprgr16@gmail.com>
2021-03-07 15:52:31 +0900

    dev: 개발중

---
+++ layouts/blank.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-main>
+    <v-container>
+      <nuxt />
+    </v-container>
+  </v-main>
+</template>
+
+<script lang="ts">
+export default {}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-sheet>
+    <v-form @sumbit.prevent="login">
+      <v-text-field
+        v-model="user.userId"
+        type="text"
+        clearable
+        autofocus
+        aria-autocomplete="username"
+      />
+      <v-text-field
+        v-model="user.password"
+        type="password"
+        clearable
+        aria-autocomplete="current-password"
+      />
+      <v-checkbox label="자동 로그인" />
+      <v-btn type="submit" block>로그인</v-btn>
+      <v-divider />
+    </v-form>
+  </v-sheet>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  layout: 'blank',
+  data: () => ({
+    user: {
+      userId: '',
+      password: '',
+    },
+  }),
+  methods: {
+    login() {},
+  },
+})
+</script>
